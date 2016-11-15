9965b1cb517a7cb3ab3d0b51533f2e57c37d7462
Valerii Kipkaev <kipkaev@kipkaev.name>
2016-11-15 15:28:57 +0600

    Init

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'vendor/autoload.php';
+use UAParser\Parser;
+
+function isMobile()
+{
+    $notMobile = array(
+          'Other',
+          'Spider',
+          'WebTV',
+          'Nintendo Wii',
+          'Nintendo DS',
+          'PlayStation 3',
+          'PlayStation Portable'
+      );
+    $parser = Parser::create();
+    $result = $parser->parse($_SERVER['HTTP_USER_AGENT']);
+    $isMobile = !in_array($result->device->family, $notMobile);
+    return $isMobile;
+}
+
+ if(isMobile()) {
+    echo file_get_contents('./mobile.html', true);
+ } else {
+    echo file_get_contents('./desktop.html', true);
+ }
